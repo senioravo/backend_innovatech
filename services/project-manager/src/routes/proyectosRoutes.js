595438b9a0b8bb4f404fab395f7991aff6b5a9fa
Diego Herrera <dieg.herrerap@duocuc.cl>
2026-05-14 09:11:42 -0400

    PM Task-07: Endpoint GET configurado.

--- services/project-manager/src/routes/proyectosRoutes.js
+++ services/project-manager/src/routes/proyectosRoutes.js
@@ -6,6 +6,11 @@ const projectController = require('../controllers/project-controller');
 const router = express.Router();
 
 router.use(authMiddleware);
+router.get(
+  '/',
+  requireRole('Gestor', 'Profesional', 'Directivo'),
+  projectController.listProjects
+);
 router.post('/', requireRole('Gestor'), projectController.createProject);
 router.delete('/:id', requireRole('Gestor'), projectController.deleteProject);
 
